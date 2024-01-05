6f7afe6d6242b4f05c5e058d93043665704a0c3c
bremixx <143969147+bremixx@users.noreply.github.com>
2024-01-05 00:02:48 -0500

    Added utility tests
    - added utility tests for checking json contents

--- tests/utility_test.py
+++ tests/utility_test.py
@@ -12,3 +12,13 @@ def test_empty_file():
 def test_normal_file():
     func = Utilities.unpack_text_file('resources\\normal_file.txt')
     assert isinstance(func, list)
+
+def test_check_urls():
+    util = Utilities()
+    func = util.check_urls('key.json', 'null')
+    assert isinstance(func, bool)
+
+def test_check_if_json_exists():
+    util = Utilities()
+    func = util.check_if_json_exists('key.json')
+    assert isinstance(func, bool)
