0979cad1a1fde21898cc7b648ea56f51c6bccf19
narqm <143969147+narqm@users.noreply.github.com>
2024-11-30 06:14:43 -0500

    Delete tests directory

--- tests/api_call_test.py
+++
@@ -1,8 +0,0 @@
-from stylus.api_call import GoogleBooksAPICall
-
-def test_normal_build_api_request():
-    gbac = GoogleBooksAPICall(r'.\resources\Think Python, sample.pdf')
-    url = gbac.build_api_request(author='Allen Downey')
-    baseline = 'https://www.googleapis.com/books/v1/volumes?' \
-        'q=think+python+inauthor:allen+downey'
-    assert url == baseline
